7047a3ccdec250ee1773a317a2217fda538f8bf1
Abhishek Junnarkar <junnarkar.abhishek@gmail.com>
2025-10-11 16:53:39 +0100

    Class notes and sample python programs

---
+++ Live class Notes/Module-Code-Examples/Module1-Project2.py
@@ -0,0 +1,35 @@
+import requests
+
+
+def get_weather(city_name, api_key):
+    """
+    Fetch current weather data for a given city using OpenWeatherMap API.
+
+    Args:
+        city_name (str): Name of the city
+        api_key (str): Your OpenWeatherMap API key
+
+    Returns:
+        dict: Weather details (temperature, description, humidity, etc.)
+    """
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': city_name,
+        'appid': api_key,
+        'units': 'metric'  # use 'imperial' for Fahrenheit
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        weather = {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'humidity': data['main']['humidity'],
+            'description': data['weather'][0]['description'].title(),
+            'wind_speed': data['wind']['speed']
+        }
+        return weather
+    else:
+        return {'error': 'City not found or invalid API key.'}
